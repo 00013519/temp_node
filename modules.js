314b1d566b93e18cad58885cffec38ba6aee66ae
00013519 <wiut.student.13519@gmail.com>
2023-03-21 13:09:32 +0500

    first commit

---
+++ modules.js
@@ -0,0 +1,7 @@
+const names = require('./names')
+const sayHi = require('./utils')
+const data = require('./alternative')
+require('./mind_granade')
+sayHi('Gulom')
+sayHi(names.john)
+sayHi(names.peter)
